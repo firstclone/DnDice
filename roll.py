ec9e04cf8d9e1ddd775f73fb5d3c16a676be6929
firstclone <first.clone@gmail.com>
2016-01-28 17:58:58 -0800

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,10 +1,25 @@
 import random
+import json
 def roll(d,s,b):
     total = 0
+    d = int(d)
+    s = int(s)
+    b = int(b)
     while d > 0:
         total = total + random.randint(1,s)
         d = d - 1
     final = total + b
-    print final
+    final = str(final)
+    output = 'Roll = '
+    output = output + final
+    return output
     
-roll(1,6,0)
+dpos = text.index('d')
+try: pluspos = text.index('+')
+except:
+    text = text + '+0'
+    pluspos = text.index('+')
+d = text[:dpos]
+s = text[dpos+1:pluspos]
+b = text[pluspos+1:]
+roll(d,s,b)
