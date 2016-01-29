16214a0265d77c321ca4975da84c558b38e783f6
firstclone <first.clone@gmail.com>
2016-01-28 22:54:27 -0800

    Update roll.py

--- roll.py
+++ roll.py
@@ -20,31 +20,31 @@ class SlashCommand():
         return dummy_handler()
         
 class DiceRoll(SlashCommand):
-    def roll(self):
+	def roll(self):
 		text = self.data.get("text",[""])[0]
-	    dpos = text.index('d')
-    	try: pluspos = text.index('+')
-	    except:
-    	    text = text + '+0'
-        	pluspos = text.index('+')
-	    d = text[:dpos]
-    	s = text[dpos+1:pluspos]
-	    b = text[pluspos+1:]
-    	total = 0
-	    d = int(d)
-    	s = int(s)
-	    b = int(b)
-    	while d > 0:
-	        total = total + random.randint(1,s)
-    	    d = d - 1
-	    final = total + b
-    	final = str(final)
-	    output = 'Roll = '
-	    output = output + final
-	    return output
+		dpos = text.index('d')
+		try: pluspos = text.index('+')
+		except:
+			text = text + '+0'
+			pluspos = text.index('+')
+		d = text[:dpos]
+		s = text[dpos+1:pluspos]
+		b = text[pluspos+1:]
+		total = 0
+		d = int(d)
+		s = int(s)
+		b = int(b)
+		while d > 0:
+			total = total + random.randint(1,s)
+			d = d - 1
+		final = total + b
+		final = str(final)
+		output = 'Roll = '
+		output = output + final
+		return output
 
-    def post_handler(self):
-        return Falcon.HTTP_200, self.roll(text)
+	def post_handler(self):
+		return Falcon.HTTP_200, self.roll(text)
 	
 
 app = falcon.API()
